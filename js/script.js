23bc5a5aee01327c1abc9b7f2a6df34825c5afe8
Ibrahim Wassouf <ib981347@dal.ca>
2022-06-09 18:14:12 -0300

    added anchor links and active functionality to menu-nav

--- js/script.js
+++ js/script.js
@@ -2,12 +2,10 @@
 //declare header nav and menu nav
 const mainNav = document.getElementById('main-nav').getBoundingClientRect();
 const menuNav = document.getElementById('menu-nav').getBoundingClientRect();
+const menuNavLinks = document.getElementById('menu').getElementsByClassName('nav-link');
+
 
 let lastScrollPosition = mainNav.bottom;
-
-
-
-
 
 document.addEventListener("scroll", function(){
     lastScrollPosition = window.scrollY;
@@ -19,11 +17,18 @@ document.addEventListener("scroll", function(){
         document.getElementById('menu-nav').classList.remove('sticky');
         document.getElementById('menu-title').classList.remove('sticky');
     }
+
+    Array.from(menuNavLinks).forEach(link => {
+        let card = document.querySelector(link.hash);
+
+        if (card.offsetTop <= lastScrollPosition + 250 && card.offsetTop + card.offsetHeight >= lastScrollPosition + 250) {
+            link.classList.add('newActive');
+
+        }
+        else {
+            link.classList.remove('newActive');
+        }
+
+    });
 });
-/*
-window.onscroll = function(){
-    if (menuNav.getBoundingClientRect().top >= 300){
-        menuNav.style.position = fixed;
-        console.log('it should have worked');
-    }
-}*/
+
